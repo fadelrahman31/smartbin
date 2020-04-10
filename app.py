b26c268212873e020a584066ac8f1a4d598b1c57
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2020-04-10 20:09:55 +0700

    done: get data tps, get data device

--- app.py
+++ app.py
@@ -27,6 +27,32 @@ def getDataTPS(id):
                 connection.close()
                 print("PostgreSQL connection is closed")
 
+def getDataDevice(id):
+    try:
+        connection = psycopg2.connect(user = "ffgdwapi",
+                                    password = "iAAuwugo4XEjormkKGmzW14-N6AWmDzD",
+                                    host = "drona.db.elephantsql.com",
+                                    port = "5432",
+                                    database = "ffgdwapi")
+
+        cursor = connection.cursor()
+
+        #run some SQL query
+        get_query = """select * from device where id_device = %s"""
+        cursor.execute(get_query, (id, ))
+        results = cursor.fetchall()
+        #print("result ", results)
+        return results
+
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while fetching data", error)
+    finally:
+        #closing database connection.
+            if(connection):
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
+
 
 app = Flask(__name__)
 
@@ -46,7 +72,10 @@ def api_data(id_tps):
 #method get retrieve data device status
 @app.route('/device/<id_device>')
 def api_device(id_device):
-    return 'Membaca data dari Device ' + id_device
+    #return 'Membaca data dari Device ' + id_device
+    data = getDataDevice(id_device)
+    result = str(data)
+    return result
 
 if __name__ == '__main__':
     app.run()
