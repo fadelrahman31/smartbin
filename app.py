45c7f25e03c3306a58aefd1a16e93342d154c8e9
Fadel Nararia Rahman <47631524+fadelrahman31@users.noreply.github.com>
2020-04-10 22:48:01 +0700

    done: post transmit data tps

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import psycopg2
-from flask import Flask
+from flask import Flask, request, json
 from dotenv import load_dotenv
 import os
 
@@ -59,6 +59,37 @@ def getDataDevice(id):
                 connection.close()
                 print("PostgreSQL connection is closed")
 
+def postDataTPS(id_tps, waktu, humidity, temp, latitude, longitude, city):
+    try:
+        connection = psycopg2.connect(user = os.getenv("DB_USER"),
+                                    password = os.getenv("DB_PASS"),
+                                    host = os.getenv("DB_HOST"),
+                                    port = os.getenv("DB_PORT"),
+                                    database = os.getenv("DATABASE")
+                                    )
+
+        cursor = connection.cursor()
+
+        #run some SQL query
+        post_query = """ INSERT INTO datatps (id_tps, waktu, humidity, temp, latitude, longitude, city) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
+        #data_insert = ('tps01', '9-April-2020', 750, 360)
+        data_insert = (id_tps, waktu, float(humidity), float(temp), float(latitude), float(longitude), city)
+        cursor.execute(post_query, data_insert)
+
+        connection.commit()
+        #count = cursor.rowcount()
+        print("Data berhasil dimasukkan kedalam tabel datatps")
+
+    except (Exception, psycopg2.Error) as error :
+        print ("gagal untuk insert data ke tabel", error)
+    finally:
+        #closing database connection.
+            if(connection):
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
+
+
 
 app = Flask(__name__)
 
@@ -70,7 +101,6 @@ def api_root():
 #method get retrieve data tps
 @app.route('/data/tps/<id_tps>')
 def api_data(id_tps):
-    #return 'Membaca data dari Server ' + id_tps
     data = getDataTPS(id_tps)
     result = str(data)
     return result
@@ -78,10 +108,31 @@ def api_data(id_tps):
 #method get retrieve data device status
 @app.route('/device/<id_device>')
 def api_device(id_device):
-    #return 'Membaca data dari Device ' + id_device
     data = getDataDevice(id_device)
     result = str(data)
     return result
+
+#method post transmit data tps
+@app.route('/data/tps/<id_tps>', methods = ['POST'])
+def api_transmit_data(id_tps):
+    if request.headers['Content-Type'] == 'application/json':
+        #respond here
+        #a = json.dumps(request.json)
+        a = (request.json)
+        #print(type(a))
+        id_tps      = a['id_tps']
+        waktu       = a['waktu']
+        humidity    = a['humidity']
+        temp        = a['temp']
+        latitude    = a['latitude']
+        longitude   = a['longitude']
+        city        = a['city']
+        #execute the Query with Fuction
+        postDataTPS(id_tps, waktu, humidity, temp, latitude, longitude, city)
+        return "Berhasil mnyimpan data dari TPS " + id_tps +" !"
+        #untuk notes, a = {'id_tps': 'tps01', 'waktu': '9-april-2020 13.45WIB', 'humidity': '33.0', 'temp': '35.5', 'latitude': '126.0','longitude': '97.0', 'city': 'Bandung'}
+    else:
+        return "415:: Unsupported Media Type!"
 
 if __name__ == '__main__':
     app.run()
